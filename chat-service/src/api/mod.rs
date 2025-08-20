d8ff02905a2e5d05780b4d17db61bda8bbedbc5e
tokin_nikita <nikitatokin2006@gmail.com>
2025-08-20 11:34:43 +0300

    update error.rs

--- chat-service/src/api/mod.rs
+++ chat-service/src/api/mod.rs
@@ -1,14 +1,12 @@
 pub mod images;
 
-use crate::api_response::ApiError;
-use axum::Json;
+use axum::{Json, http::StatusCode, response::IntoResponse};
 use serde_json::json;
 
 pub async fn ping() -> Json<serde_json::Value> {
     Json(json!({"ping": "pong!"}))
 }
 
-pub async fn not_found() -> ApiError {
-    ApiError::NotFound("Url not found".to_owned())
+pub async fn not_found() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, Json(json!({"error": "Not found"})))
 }
-
