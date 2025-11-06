def3e0e9b88816be44ff0b733aae9d8583bba000
tokin_nikita <nikitatokin2006@gmail.com>
2025-11-06 16:48:07 +0300

    add test for ping and not_found endpoints

---
+++ service/tests/integration_test.rs
@@ -0,0 +1,36 @@
+use axum::{
+    body::Body,
+    http::{Request, StatusCode}
+};
+use http_body_util::BodyExt;
+use serde_json::{Value, json};
+use service::ServerBuilder;
+use tower::ServiceExt;
+
+#[tokio::test]
+async fn test_ping() {
+    dotenvy::dotenv().ok();
+    let app = ServerBuilder::init_router().await;
+
+    let response = app
+        .oneshot(Request::builder().uri("/ping").body(Body::empty()).unwrap())
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body = serde_json::from_slice::<Value>(&body).unwrap();
+    assert_eq!(body, json!({"ping": "pong!"}));
+}
+
+#[tokio::test]
+async fn test_not_found() {
+    dotenvy::dotenv().ok();
+    let app = ServerBuilder::init_router().await;
+    let response = app
+        .oneshot(Request::builder().uri("/does-not-exist").body(Body::empty()).unwrap())
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+}
